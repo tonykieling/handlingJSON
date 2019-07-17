d294b37213975eb546a10ef0cfc88ce40622f1b2
Tony Kieling <tony.kieling@gmail.com>
2019-07-17 15:38:49 -0700

    method is done and working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    pLoad: false,
+    data: ""
+  }
+
+  componentDidMount() {
+    this.grabData();  
+  }
+
+  grabData = async () => {
+    //data is coming from jsonplaceholder
+    const addr = "https://jsonplaceholder.typicode.com/users";
+    const rData = await fetch(addr);
+    const fData = await rData.json();
+    await this.setState({
+      pLoad : true,
+      data  : fData
+    });
+  }
+
+  //this is the method that arrange the data in a HTMl format
+  presentation = (arr) => {
+    return arr.map(item =>
+      <div key = {item.id}>
+        {/* let's assume that name from item is gonna be the title */}
+        <h3>Title: {item.name}</h3>
+        {/* let's assume that email from item is gonna be the excerpt */}
+        <p>Excerpt: {item.email}</p>
+        <br></br>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>CTO.ai</h1>
+        {this.state.pLoad ? this.presentation(this.state.data) : null}
+      </div>
+    )
+  }
 }
 
-export default App;
